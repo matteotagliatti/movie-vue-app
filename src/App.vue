2304844c8ab9516a70ca517a064e06f707c49705
Matteo Tagliatti <matteotagliatti@gmail.com>
2022-03-16 15:37:12 +0100

    firsrt commit. Init app with first elements

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <header>
+    <router-link to="/">
+      <h1>Movie<span>Vue</span>App</h1>
+    </router-link>
+  </header>
+  <main>
+    <router-view />
+  </main>
+</template>
+
+<style lang="scss">
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: "Fira Sans", sans-serif;
+  background-color: #131418;
+
+  &::selection {
+    background: transparentize(#43b883, 0.5);
+  }
+}
+
+h1 {
+  color: #fff;
+  font-size: 28px;
+
+  span {
+    color: #42b883;
+  }
+}
+
+a {
+  text-decoration: none;
+}
+
+header {
+  padding: 10px 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="home">
+    <div class="card">
+      <router-link to="/movie/tt0409591">
+        <img
+          src="https://posterspy.com/wp-content/uploads/2020/04/DuneFINAL-1200x609.jpg"
+          alt="feature-img"
+        />
+        <div class="detail">
+          <h3>Dune</h3>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque
+            officiis blanditiis inventore.
+          </p>
+        </div>
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+.home {
+  .card {
+    position: relative;
+  }
+
+  img {
+    display: block;
+    width: 100%;
+    height: 300px;
+    object-fit: cover;
+    position: relative;
+  }
+
+  .detail {
+    position: absolute;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: rgba(0, 0, 0, 0.6);
+    padding: 16px;
+    z-index: 1;
+    color: #fff;
+
+    h3 {
+      margin-bottom: 16px;
+    }
+  }
+}
+</style>
