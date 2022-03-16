bf47df494532796ec900a6e0c87ccd41206c4d0c
Matteo Tagliatti <matteotagliatti@gmail.com>
2022-03-16 17:40:52 +0100

    movie detail page

--- src/views/MovieDetail.vue
+++ src/views/MovieDetail.vue
@@ -1,9 +1,55 @@
 <template>
-  <div class="movie-detail">Detail {{ $route.params.id }}</div>
+  <div class="movie-detail">
+    <h2>{{ movie.Title }}</h2>
+    <p>{{ movie.Year }}</p>
+    <img :src="movie.Poster" alt="Movie Poster" class="featured-img" />
+    <p>{{ movie.Plot }}</p>
+  </div>
 </template>
 
 <script>
-export default {};
+import { ref, onBeforeMount } from "vue";
+import { useRoute } from "vue-router";
+import env from "@/env.js";
+export default {
+  setup() {
+    const movie = ref({});
+    const route = useRoute();
+    onBeforeMount(() => {
+      fetch(
+        `http://www.omdbapi.com/?apikey=${env.apikey}&i=${route.params.id}&plot=full`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          movie.value = data;
+        });
+    });
+    return {
+      movie,
+    };
+  },
+};
 </script>
 
-<style></style>
+<style lang="scss">
+.movie-detail {
+  padding: 1rem;
+
+  h2 {
+    font-size: 1.5rem;
+    font-weight: 600;
+  }
+
+  .featured-img {
+    display: block;
+    max-width: 200px;
+    margin-bottom: 1rem;
+  }
+
+  p {
+    font-size: 1rem;
+    line-height: 1.4;
+    margin-bottom: 1rem;
+  }
+}
+</style>
